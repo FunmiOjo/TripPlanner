895d6600ffdaab1557fb0cbab33bedf577d3448f
Tricia Lobo <tricia.lobo@gmail.com>
2018-08-07 17:17:44 -0400

    m

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const mapboxgl = require('mapbox-gl');
+const marker = require('marker.js');
 
-mapboxgl.accessToken = 'pk.eyJ1IjoidHJpY2lhbG9ibyIsImEiOiJjamtrNWF3bzgxbXlvM3BtbHh6aGNuZGhmIn0.K0wJwNaqqGTA5JzRzrZKdw';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoidHJpY2lhbG9ibyIsImEiOiJjamtrNWF3bzgxbXlvM3BtbHh6aGNuZGhmIn0.K0wJwNaqqGTA5JzRzrZKdw';
 
 const map = new mapboxgl.Map({
   container: 'map',
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10"
+  style: 'mapbox://styles/mapbox/streets-v10',
 });
 
-//module.exports = map;
+module.exports = map;
---
+++ src/marker.js
@@ -0,0 +1,18 @@
+
+const mapboxgl = require('mapbox-gl');
+const markerDomEl = document.createElement('div');
+markerDomEl.style.width = '32px';
+markerDomEl.style.height = '39px';
+
+function markerBuild (type, arrCoords) {
+    if (type==='Activity') 
+    markerDomEl.style.backgroundImage = http://i.imgur.com/WbMOfMl.png;
+    else if (type==='Hotel') markerDomEl.style.backgroundImage =  http://i.imgur.com/D9574Cu.png;
+    else markerDomEl.style.backgroundImage = http://i.imgur.com/cqR6pUI.png;
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
+}
+
+
+
+module.exports = markerBuild;
